b0428265a1b601029b5a5d660cfe0a50a9f037cc
Jerod Gawne <jerodgawne@gmail.com>
2019-07-31 18:55:39 -0400

    Update phantom-api-client
    set run_automation to default to False;
    changes to get_container_count() to handle request_id;
    move performance tests to tests.extras;
    update tests to handle request_id

--- phantom_api_client/client.py
+++ phantom_api_client/client.py
@@ -20,20 +20,21 @@ If not, see <https://www.mongodb.com/licensing/server-side-public-license>."""
 
 import asyncio
 import logging
-from typing import Optional, Union
+from typing import List, Optional, Union
+from uuid import uuid4
 
 import aiohttp as aio
 import ujson
 
 from base_api_client import BaseApiClient, Results
-from phantom_api_client.models import ContainerFilter
+from phantom_api_client.models import ContainerFilter, ContainerRequest
 
 logger = logging.getLogger(__name__)
 
 
 class PhantomApiClient(BaseApiClient):
     """Phantom API Client"""
-    SEM: int = 10  # This defines the number of parallel async requests to make.
+    SEM: int = 15  # This defines the number of parallel async requests to make.
 
     def __init__(self, cfg: Union[str, dict], sem: Optional[int] = None):
         """Initializes Class
@@ -56,6 +57,7 @@ class PhantomApiClient(BaseApiClient):
 
     async def get_container_count(self, filter: Optional[ContainerFilter] = ContainerFilter()) -> Results:
         filter.page_size = 1  # Only need one record to get the total count
+        filter.page = 0
 
         async with aio.ClientSession(headers=self.header, json_serialize=ujson.dumps) as session:
             logger.debug('Getting container count...')
@@ -63,16 +65,18 @@ class PhantomApiClient(BaseApiClient):
             tasks = [asyncio.create_task(self.request(method='get',
                                                       end_point='/container',
                                                       session=session,
-                                                      params=filter.dict))]
-            results = await asyncio.gather(*tasks)
+                                                      request_id=uuid4().hex,
+                                                      params=filter.dict()))]
+
+            results = Results(data=await asyncio.gather(*tasks))
 
             logger.debug('-> Complete.')
 
         await session.close()
 
-        return await self.process_results([{'container_count': results[0]['count']}])
+        return await self.process_results(results)
 
-    async def get_containers(self, filter: Optional[ContainerFilter] = ContainerFilter()):
+    async def get_containers(self, filter: Optional[ContainerFilter] = ContainerFilter()) -> Results:
         if filter.limit:
             limit = filter.limit
             del filter.limit
@@ -88,7 +92,26 @@ class PhantomApiClient(BaseApiClient):
                 tasks.append(asyncio.create_task(self.request(method='get',
                                                               end_point='/container',
                                                               session=session,
-                                                              params=filter.dict)))
+                                                              request_id=uuid4().hex,
+                                                              params=filter.dict())))
+
+            results = await asyncio.gather(*tasks)
+
+            logger.debug('-> Complete.')
+
+        await session.close()
+
+        return await self.process_results(results, 'data')
+
+    async def create_containers(self, containers: Union[List[ContainerRequest], ContainerRequest]) -> Results:
+
+        async with aio.ClientSession(headers=self.header, json_serialize=ujson.dumps) as session:
+            logger.debug('Creating container(s)...')
+
+            tasks = [asyncio.create_task(self.request(method='post',
+                                                      end_point='/container',
+                                                      session=session,
+                                                      json=c.dict())) for c in containers]
 
             results = await asyncio.gather(*tasks)
 
--- tests/test_containers.py
+++ tests/test_containers.py
@@ -21,7 +21,6 @@ import time
 
 import pytest
 from os import getenv
-from os.path import realpath
 
 from base_api_client import bprint, Results, tprint
 from phantom_api_client import PhantomApiClient
@@ -32,9 +31,10 @@ from phantom_api_client.models import ContainerFilter
 async def test_get_container_count():
     ts = time.perf_counter()
 
-    bprint('Test: Get ContainerRequest Count')
+    bprint('Test: Get Container Count')
     async with PhantomApiClient(cfg=f'{getenv("CFG_HOME")}/phantom_api_client.toml') as pac:
         results = await pac.get_container_count()
+        print(results)
 
         assert type(results) is Results
         assert len(results.success) >= 1
@@ -51,14 +51,7 @@ async def test_get_containers():
 
     bprint('Test: Get Containers')
     async with PhantomApiClient(cfg=f'{getenv("CFG_HOME")}/phantom_api_client.toml') as pac:
-        filter = ContainerFilter(page_size=100)
-        # container_count = (await pac.get_container_count()).success[0]['container_count']
-        # all_results = await pac.get_containers(ContainerFilter(page_size=container_count + 100))
-
-        results = await pac.get_containers(filter)
-
-        # difference = [i for i in results.success if i not in all_results.success]
-        # print('Results difference:', difference)
+        results = await pac.get_containers()
 
         ids = len(list(set([k['id'] for k in results.success])))
         print(f'Results: {len(results.success)} == Ids: {ids}?')
@@ -67,48 +60,19 @@ async def test_get_containers():
         assert len(results.success) >= 1
         assert not results.failure
         assert len(results.success) == ids  # Ensure no duplicates
-        # If a new container is created between the get_containers calls these will not match up
-        # assert container_count == len(results.success)
 
-        print(f'Fetched {len(results.success)} containers.')
         tprint(results, top=5)
 
     bprint(f'-> Completed in {(time.perf_counter() - ts):f} seconds.')
 
 
 @pytest.mark.asyncio
-async def test_get_containers_performance_analysis():
-    ts = time.perf_counter()
-    bprint('Test: Get Containers Performance Analysis')
-    # Semaphore, PageSize
-    semaphores = range(1, 100, 5)
-    page_sizes = range(1, 100, 10)
-    stats = []
-
-    for semaphore in semaphores:
-        for page_size in range(1, 100, 10):
-            async with PhantomApiClient(cfg=f'{getenv("CFG_HOME")}/phantom_api_client.toml', sem=semaphore) as pac:
-                tts = time.perf_counter()
-                results = await pac.get_containers(ContainerFilter(page_size=page_size))
-                recs = len(results.success)
-                dur = time.perf_counter() - tts
-                stat = f'| {semaphore}\t| {page_size}\t| {recs}\t| {dur:f}\t| {recs / dur:f}\t|'
-                stats.append(stat)
-                print(stat)
-
-    with open(realpath('./data/get_containers_stats_2019-07-30.txt')) as sfile:
-        sfile.writelines(stats)
-
-    bprint(f'-> Completed in {(time.perf_counter() - ts) / 60:f} minutes.')
-
-# todo: test
-@pytest.mark.asyncio
 async def test_get_container_count_filtered():
     ts = time.perf_counter()
 
     bprint('Test: Get ContainerRequest Count')
     async with PhantomApiClient(cfg=f'{getenv("CFG_HOME")}/phantom_api_client.toml') as pac:
-        f = {'_filter_name__icontains': 'mss'}
+        f = {'_filter_name__icontains': '"test"'}
         results = await pac.get_container_count(ContainerFilter(filter=f))
 
         assert type(results) is Results
@@ -120,32 +84,25 @@ async def test_get_container_count_filtered():
     bprint(f'-> Completed in {(time.perf_counter() - ts):f} seconds.')
 
 
-# todo: test
 @pytest.mark.asyncio
 async def test_get_containers_filtered():
     ts = time.perf_counter()
 
     bprint('Test: Get Containers')
     async with PhantomApiClient(cfg=f'{getenv("CFG_HOME")}/phantom_api_client.toml') as pac:
-        container_count = (await pac.get_container_count()).success[0]['container_count']
-        # all_results = await pac.get_containers(ContainerFilter(page_size=container_count + 100))
+        f = {'_filter_name__icontains': '"test"'}
+        results = await pac.get_containers(ContainerFilter(filter=f))
+        print('results:')
+        print(*results, sep='\n')
 
-        results = await pac.get_containers()
+        # ids = len(list(set([k['id'] for k in results.success])))
+        # print(f'Results: {len(results.success)} == Ids: {ids}?')
 
-        # difference = [i for i in results.success if i not in all_results.success]
-        # print('Results difference:', difference)
+        # assert type(results) is Results
+        # assert len(results.success) >= 1
+        # assert not results.failure
+        # assert len(results.success) == ids  # Ensure no duplicates
 
-        ids = len(list(set([k['id'] for k, v in results.success.items()])))
-        print(f'Results: {len(results.success)} == Ids: {ids}?')
-
-        assert type(results) is Results
-        assert len(results.success) >= 1
-        assert not results.failure
-        assert len(results.success) == ids  # Ensure no duplicates
-        # If a new container is created between the get_containers calls these will not match up
-        # assert container_count == len(results.success)
-
-        print(f'Fetched {len(results.success)} containers.')
-        tprint(results, top=5)
+        # tprint(results, top=5)
 
     bprint(f'-> Completed in {(time.perf_counter() - ts):f} seconds.')
